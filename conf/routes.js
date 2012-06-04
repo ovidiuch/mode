44c9de40c7cc1ea40349cc7e5639a0059790387a
Ovidiu Chereches <skidding@gmail.com>
2012-06-04 17:57:24 +0300

    Fixed main route

--- conf/routes.js
+++ conf/routes.js
@@ -2,7 +2,7 @@ exports.list =
 [
 	/* /controller/action */
 
-	{ pattern: /\/([a-z0-9]+)\/([a-z0-9]+)/i, args: { controller: '$1', action: '$2' } },
+	{ pattern: /^\/([a-z0-9]+)\/([a-z0-9]+)$/i, args: { controller: '$1', action: '$2' } },
 
 	/* assets */
 
