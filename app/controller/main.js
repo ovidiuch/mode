458d46bd896e3e85e92f3ef7e2fbc8212aba14f5
Ovidiu Chereches <skidding@gmail.com>
2012-06-09 15:24:20 +0300

    Added static property to classes

--- app/controller/main.js
+++ app/controller/main.js
@@ -2,8 +2,12 @@ exports.super = 'controller/base';
 
 exports.class = function(mode)
 {
+	this.static.counter = 0;
+
 	this.action.index = function(callback)
 	{
-		callback('Hmm...');
+		this.static.counter++;
+
+		callback('Hello... ' + this.static.counter);
 	};
 };
