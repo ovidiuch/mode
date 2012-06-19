07057074a0f1616f75260e3416b15a02996859a4
Ovidiu Chereches <skidding@gmail.com>
2012-06-19 12:32:03 +0300

    Added callback type check in app controller

--- app/controller/app.js
+++ app/controller/app.js
@@ -7,9 +7,12 @@ exports.class = function(mode)
 	this.action.init = function(callback)
 	{
 		this.args.layout = 'default';
-		
+
 		this.args.title = 'Hello World!';
 
-		callback();
+		if(typeof(callback) == 'function')
+		{
+			callback();
+		}
 	}
 };
