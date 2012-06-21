930b2d69a0c49dc9497806f998543f8c1b502cfd
Ovidiu Chereches <skidding@gmail.com>
2012-06-21 18:33:26 +0300

    Removed template from main repository

--- template/app/controller/error.js
+++
@@ -1,9 +0,0 @@
-exports.super = 'controller/base';
-
-exports.class = function(mode)
-{
-	this.action.load = function(callback)
-	{
-		callback('ERROR: ' + this.args.message);
-	};
-};
--- template/start.js
+++
@@ -1,5 +0,0 @@
-var mode = require('mode');
-
-mode.setup({ base: __dirname });
-
-mode.start(mode.settings.server);
