09480368074dae61467c0ddbac8e8037a0257a21
Ovidiu Chereches <skidding@gmail.com>
2012-09-29 18:42:47 +0300

    Added first test for the extension module #5

---
+++ test/test-extension.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+var extension = require('../lib/extension.js');
+
+var baseQuery = '/path/to/something';
+var suffixes = [
+  // Query has nothing succedding the extension
+  '',
+  // Query has a query string
+  '?query=string&one=1',
+  // Query has a hashtag
+  '#hash-tag',
+  // Query has both a query string and a hastag
+  '?query=string&one=1#hash-tag'
+];
+suffixes.forEach(function(suffix) {
+  var query = baseQuery + '.ext' + suffix;
+
+  // Removing existing extension
+  query = extension.strip(query);
+  assert.equal(query, baseQuery + suffix);
+
+  // Extract inexisting extension
+  assert.equal(extension.extract(query), '');
+
+  // Strip inexisting extension
+  assert.equal(extension.strip(query), query);
+
+  // Add extension when none is present
+  query = extension.change(query, 'js');
+  assert.equal(query, baseQuery + '.js' + suffix);
+
+  // Change existing extension
+  query = extension.change(query, 'html');
+  assert.equal(query, baseQuery + '.html' + suffix);
+
+  // Extract current extension
+  assert.equal(extension.extract(query), 'html');
+});
